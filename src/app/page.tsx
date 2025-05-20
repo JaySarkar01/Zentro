d1e3d05cec8371067d9c70f5d1a97c11105d6b71
JaySarkar01 <jaysarkari9@gmail.com>
2025-05-20 14:37:21 +0530

    commit a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-"use client";
 import React from 'react';
+import stripe from 'stripe';
 
-export default function Home() {
+export default async function Home() {
+
+  const products = await stripe.products.list({
+    expand:["data.default_price"],
+    limit: 5
+  });
+
+  console.log(products);
   return (
     <div>
       <h1>Home</h1>
